59608b67bc719940c613bcd604cbd6ba3f4be6ff
Zetzher <j.abasolo.agenjo@gmail.com>
2020-10-29 17:24:20 +0100

    changing url for cors

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ app.use(
   })
 );
   app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000', 'https://iron-league.firebaseapp.com', 'https://iron-league.web.app');
+  res.setHeader('Access-Control-Allow-Origin', 'https://iron-league.firebaseapp.com', 'https://iron-league.web.app');
   res.setHeader('Access-Control-Allow-Methods', 'GET, PUT, POST OPTIONS, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.setHeader('Access-Control-Allow-Credentials', true);
