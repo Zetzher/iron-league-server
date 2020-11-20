1899aa4b2b4b77276eef8091b52188b81ab04980
Zetzher <j.abasolo.agenjo@gmail.com>
2020-11-20 11:44:23 +0100

    done

--- routes/login.js
+++ routes/login.js
@@ -17,32 +17,32 @@ const {
 
 
 router.post(
-    "/",
-    //isNotLoggedIn(),
-    validationLoggin(),
-    async (req, res, next) => {
-      const { email, password } = req.body;
-      try {
-        // revisa si el usuario existe en la BD
-        const user = await User.findOne({ email });
-        // si el usuario no existe, pasa el error al middleware error usando next()
-        if (!user) {
-          next(createError(404));
-        }
-        // si el usuario existe, hace hash del password y lo compara con el de la BD
-        // loguea al usuario asignando el document a req.session.currentUser, y devuelve un json con el user
-        else if (bcrypt.compareSync(password, user.password,)) {
-          req.session.currentUser = user;
-          res.status(200).json(user);
-          return;
-        } else {
-          next(createError(401));
-        }
-      } catch (error) {
-        next(error);
+  "/",
+  //isNotLoggedIn(),
+  validationLoggin(),
+  async (req, res, next) => {
+    const { email, password } = req.body;
+    try {
+      // revisa si el usuario existe en la BD
+      const user = await User.findOne({ email });
+      // si el usuario no existe, pasa el error al middleware error usando next()
+      if (!user) {
+        next(createError(404));
       }
+      // si el usuario existe, hace hash del password y lo compara con el de la BD
+      // loguea al usuario asignando el document a req.session.currentUser, y devuelve un json con el user
+      else if (bcrypt.compareSync(password, user.password,)) {
+        req.session.currentUser = user;
+        res.status(200).json(user);
+        return;
+      } else {
+        next(createError(401));
+      }
+    } catch (error) {
+      next(error);
     }
-  );
+  }
+);
 
   
 
