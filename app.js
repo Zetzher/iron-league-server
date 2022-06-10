b746dd571d129c8f3d166cf6673b606b11d88fdf
zetzher <j.abasolo.agenjo@gmail.com>
2022-06-10 09:28:01 +0200

    changed url cors

--- app.js
+++ app.js
@@ -1,102 +1,108 @@
-require('dotenv').config();
-const colors = require('colors');
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const session = require('express-session');
-const MongoStore = require('connect-mongo')(session);
-const bcrypt = require('bcrypt');
-const multer = require('multer');
-const favicon = require('serve-favicon')
-const cors = require('cors')
+require("dotenv").config();
+const colors = require("colors");
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+const bcrypt = require("bcrypt");
+const multer = require("multer");
+const favicon = require("serve-favicon");
+const cors = require("cors");
 
-
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose
+  .connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
-    useFindAndModify: false
-})
-    .then(x => {
-        console.log(
-            `Connected to Mongo! Database name: "${x.connections[0].name.rainbow}"`
-        );
-    })
-    .catch(err => {
-        console.error('Error connecting to mongo', err);
-    });
-const app = express()
+    useFindAndModify: false,
+  })
+  .then((x) => {
+    console.log(
+      `Connected to Mongo! Database name: "${x.connections[0].name.rainbow}"`
+    );
+  })
+  .catch((err) => {
+    console.error("Error connecting to mongo", err);
+  });
+const app = express();
 
 app.use(
-    session({
-      store: new MongoStore({
-        mongooseConnection: mongoose.connection,
-        ttl: 24 * 60 * 60, // 1 day
-      }),
-      secret: process.env.SECRET_SESSION,
-      resave: true,
-      saveUninitialized: true,
-      cookie: {
-        maxAge: 24 * 60 * 60 * 1000,
-      },
-    })
-  );
+  session({
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+      ttl: 24 * 60 * 60, // 1 day
+    }),
+    secret: process.env.SECRET_SESSION,
+    resave: true,
+    saveUninitialized: true,
+    cookie: {
+      maxAge: 24 * 60 * 60 * 1000,
+    },
+  })
+);
 
-  // CORS MIDDLEWARE SETUP
+// CORS MIDDLEWARE SETUP
 app.use(
   cors({
     credentials: true,
-    origin: ['http://localhost:3000', 'https://ironleaguehack.firebaseapp.com', 'https://ironleaguehack.web.app'],
+    origin: [
+      "https://ironleaguehack.firebaseapp.com",
+      "http://localhost:3000",
+      "https://ironleaguehack.web.app",
+    ],
   })
 );
-  app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000', 'https://ironleaguehack.firebaseapp.com', 'https://ironleaguehack.web.app');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, PUT, POST OPTIONS, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.setHeader('Access-Control-Allow-Credentials', true);
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://ironleaguehack.firebaseapp.com",
+    "http://localhost:3000",
+    "https://ironleaguehack.web.app"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, PUT, POST OPTIONS, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
   next();
 });
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-
-let index = require('./routes/index');
-let signup = require ('./routes/signup');
-let login = require ('./routes/login');
-let logout = require ('./routes/logout');
-let objetos = require ('./routes/objetos');
-let perfil = require ('./routes/perfil');
-let players = require ('./routes/player');
-let enemigos = require ('./routes/enemigos');
+let index = require("./routes/index");
+let signup = require("./routes/signup");
+let login = require("./routes/login");
+let logout = require("./routes/logout");
+let objetos = require("./routes/objetos");
+let perfil = require("./routes/perfil");
+let players = require("./routes/player");
+let enemigos = require("./routes/enemigos");
 let user = require("./routes/user");
 let final = require("./routes/final");
 
-
-
-app.use('/',index);
-app.use ('/signup',signup)
-app.use ('/login', login)
-app.use ('/logout', logout)
-app.use ('/objetos', objetos)
-app.use ('/perfil', perfil)
-app.use ('/enemigos', enemigos)
-app.use ('/players', players)
-app.use ("/user", user)
-app.use ("/final", final)
-
-
-
-
-
+app.use("/", index);
+app.use("/signup", signup);
+app.use("/login", login);
+app.use("/logout", logout);
+app.use("/objetos", objetos);
+app.use("/perfil", perfil);
+app.use("/enemigos", enemigos);
+app.use("/players", players);
+app.use("/user", user);
+app.use("/final", final);
 
 module.exports = app;
-
